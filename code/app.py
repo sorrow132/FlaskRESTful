82d425599cfc6ccb8115a73c1d6dfae8bb0bede4
nanemo <nanemomain@gmail.com>
2021-06-16 12:39:55 +0300

    initial coomit

--- code/app.py
+++ code/app.py
@@ -28,6 +28,22 @@ class Item(Resource):
         items.append(item)
         return item, 201
 
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}
+
+    # update item or add new one
+    def put(self, name):
+        data = Item.parser.parse_args()
+        item = filter(lambda x: x['name'] == name, items, None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
 
 class ItemList(Resource):
     def get(self):
