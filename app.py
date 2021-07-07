d677ed4ec5f311726a4c64e4458a5c8b03f5e160
nanemo <nanemomain@gmail.com>
2021-07-07 18:56:51 +0300

    Fixed not importing db when running the app.

--- app.py
+++ app.py
@@ -11,14 +11,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
-app.secret_key = '????'
+app.secret_key = 'nanemo'
 api = Api(app)
-
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 
 jwt = JWT(app, authenticate, identity)  # /auth
 
